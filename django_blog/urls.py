02eb0df08f1e6b39dc1cc76739900567ec33762c
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-03 21:04:10 +0000

    Initial commit & setup django

---
+++ django_blog/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('projects.urls'))
+]
