e4e4554003fea42d99c4e6b747672ce17cbb5852
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-05 17:51:10 +0000

    Adding and loading static files

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,8 @@ class Project(models.Model):
 
     title = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(
+        null=True, blank=True, default='default.jpg')
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
@@ -16,14 +18,13 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    
-    
+
     def __str__(self):
         return self.title
-    
+
 
 class Review(models.Model):
-    
+
     VOTE_TYPE = (
         ('up', 'Up Vote'),
         ('down', 'Down Vote'),
@@ -35,19 +36,17 @@ class Review(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    
-    
+
     def __str__(self):
         return self.value
-    
+
 
 class Tag(models.Model):
-    
+
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    
-    
+
     def __str__(self):
         return self.name
