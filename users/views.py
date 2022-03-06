18a1404c4689595d92511a5da616af676b39c618
Alex Nypels <nypels.alexandre@outlook.com>
2022-03-06 21:01:24 +0000

    Adding single profile theme

--- users/views.py
+++ users/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 def profiles(request):
+    profiles = Profile.objects.all()
+    context = {
+        'profiles': profiles
+    }
+    return render(request, 'users/profiles.html', context)
 
-    return render(request, 'users/profiles.html')
+
+def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {
+        'profile': profile
+    }
+    return render(request, 'users/user_profile.html', context)
